a4f504368a7dea5afcd5da51d8dbc447799903f3
Guillaume <guillaume.bied@ensae.fr>
2018-01-19 19:02:21 +0100

    Premières lignes APnews
    Code pour retirer le début de la base APnews

---
+++ APnews.R
@@ -0,0 +1,20 @@
+library(quanteda) # Facilite le traitement de données textuelles.
+library(tidyr) # Séparation des bigrams dans les dataframe; pas élégant...
+
+data <- readLines("C:/Users/Guillaume/Desktop/apnews/apnews.dat", n=100) # Sous-échantillon
+
+unigram <- dfm(data, remove = stopwords("english"), remove_punct = TRUE) 
+  # Traitement du texte (ponctuations, etc...) et comptage des mots
+
+unigram <- data.frame(as.matrix(colSums(unigram))) # Matrice associant à chaque mot son nombre d'occurences
+unigram <- cbind(rownames(unigram), data.frame(unigram, row.names=NULL))
+colnames(unigram)[1] <- "words"
+colnames(unigram)[2] <- "counts"
+
+bigram <- dfm(data, remove = stopwords("english"), remove_punct = TRUE, ngrams=2) 
+bigram <- data.frame(as.matrix(colSums(bigram))) 
+bigram <- cbind(rownames(bigram), data.frame(bigram, row.names=NULL))
+colnames(bigram)[1] <- "words"
+colnames(bigram)[2] <- "counts"
+bigram <- bigram %>% separate(words, c("one", "two"), "_")
+
